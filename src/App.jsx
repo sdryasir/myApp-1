6a7f1ab64058820a5318e33e641943b15b6e698e
Yasir Ali <sdr.yasir@gmail.com>
2025-08-21 05:21:01 -0700

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
+
+// 1 - you can not have unclosed tags in XML
+// 2 - We can write custom tags in XML
+// 3 - we will use className instead of class for writing CSS 
+
+
+
+
+
